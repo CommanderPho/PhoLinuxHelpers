9d4cd544f941262ff5afd894f2115e9eb34faa66
Pho Hale <halechr@umich.edu>
2024-10-02 15:41:16 -0400

    GL

--- sources/greatlakes_Helpers/greatlakes_MOUNT.sh
+++ sources/greatlakes_Helpers/greatlakes_MOUNT.sh
@@ -29,14 +29,15 @@
 # ln -s /nfs/turbo/umms-kdiba/ ~/cloud/turbo
 
 
-module load archivetar rclone
+module load rclone
+# module load archivetar
 export CLOUD_BASE_PATH="${HOME}/cloud" # for greatlakes
 source "$HOME/repos/PhoLinuxHelpers/sources/mounting_cloud_drives.sh" # /home/halechr/repos/PhoLinuxHelpers
 # mount_all_cloud_drives "$CLOUD_BASE_PATH" # run mount_all_cloud_drives
-mount_cloud_drive "Diba_Lab_Shared_GDrive:" "${CLOUD_BASE_PATH}/GDrive_Diba_Shared" "${CLOUD_BASE_PATH}/logs/rclone_deamon_GDrive_Diba_Shared.log"
+# mount_cloud_drive "Diba_Lab_Shared_GDrive:" "${CLOUD_BASE_PATH}/GDrive_Diba_Shared" "${CLOUD_BASE_PATH}/logs/rclone_deamon_GDrive_Diba_Shared.log"
 mount_cloud_drive "Diba_Lab_UMich_Dropbox:" "${CLOUD_BASE_PATH}/Dropbox_Diba_Shared" "${CLOUD_BASE_PATH}/logs/rclone_deamon_Dropbox_Diba_Shared.log"
 # Personal UMich GDrive
-mount_cloud_drive "halechr_GDrive:" "${CLOUD_BASE_PATH}/GDrive_Pho_UMich" "${CLOUD_BASE_PATH}/logs/rclone_deamon_GDrive_Pho_UMich.log"
+# mount_cloud_drive "halechr_GDrive:" "${CLOUD_BASE_PATH}/GDrive_Pho_UMich" "${CLOUD_BASE_PATH}/logs/rclone_deamon_GDrive_Pho_UMich.log"
 
 # on GreatLakes, turbo is accessible at:
 # ln -s /nfs/turbo/umms-kdiba/ ~/cloud/turbo
