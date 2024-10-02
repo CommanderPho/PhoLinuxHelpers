0b3546590ec88e5e8c7c978e1a8c0d701d14aa46
Pho Hale <PhoHale@gmail.com>
2024-10-02 16:08:35 -0400

    ff

--- sources/greatlakes_Helpers/file_lists.sh
+++ sources/greatlakes_Helpers/file_lists.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Function to create a zip backup for each directory
+# Function to export a CSV of filenames and their paths from a directory
 export_filelist_csv_of_directory() {
     local folder_path=$1
 
-	# Define a date string in the format YYYY-MM-DD
-	DATE=$(date +"%Y-%m-%d")
+    # Define a date string in the format YYYY-MM-DD
+    DATE=$(date +"%Y-%m-%d")
 
-    # Change to the parent folder
-    cd "$folder_path" || exit 1
+    local csv_filename="${DATE}_$(basename "${folder_path}").csv"
 
-	local csv_filename="${DATE}_$(basename "${folder_path}").csv"
-
-	echo "\"Filename\",\"Path\"" > "${csv_filename}"
-	find "$folder_path" -type f -print0 | while IFS= read -r -d '' file; do
-		echo "\"$(basename "$file")\",\"$(dirname "$file")\""
-	done >> "${csv_filename}"
-	echo "CSV filelist created for $folder_path: $csv_filename"
-    
+    echo "\"Filename\",\"Path\"" > "${csv_filename}"
+    find "$folder_path" -type f -print0 | while IFS= read -r -d '' file; do
+        echo "\"$(basename "$file")\",\"$(dirname "$file")\""
+    done >> "${csv_filename}"
+    echo "CSV filelist created for $folder_path: $csv_filename"
 }
 
 
